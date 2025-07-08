c9cbeefb815d1f72fdd8d97fc56231e6ca78697b
EC2 Default User <ec2-user@ip-172-31-36-62.ap-south-1.compute.internal>
2025-07-08 09:45:34 +0000

    Converted project to WAR packaging and updated main class

--- src/test/java/com/example/AptItSolutions/AptItSolutionsApplicationTests.java
+++ src/test/java/com/example/AptItSolutions/AptItSolutionsApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.AptItSolutions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.example.AptItSolutions.AptitsApplication.class)
 class AptItSolutionsApplicationTests {
 
 	@Test
