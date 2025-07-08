155601624ebf8813b4236d9f74c0ef427412273a
siri-priyanka <120650732+siri-priyanka@users.noreply.github.com>
2025-07-08 17:18:50 +0530

    Update and rename AptItSolutionsApplicationTests.java to AptitsApplication.java

---
+++ src/test/java/com/example/AptItSolutions/AptitsApplication.java
@@ -0,0 +1,12 @@
+package com.example.AptItSolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptitsApplication.class, args);
+    }
+}
