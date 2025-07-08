4e23e5a2915820ae2e840f655ca62f8fc399dbad
EC2 Default User <ec2-user@ip-172-31-36-62.ap-south-1.compute.internal>
2025-07-08 11:51:36 +0000

    Commiting the new move

--- src/main/java/com/example/AptItSolutions/AptitsApplication.java
+++ src/main/java/com/example/AptItSolutions/AptitsApplication.java
@@ -1,18 +1,10 @@
-package com.aptits;
+package com.example.AptItSolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AptitsApplication extends SpringBootServletInitializer {
-
-    // This method is required for WAR deployment to external Tomcat
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AptitsApplication.class);
-    }
+public class AptitsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AptitsApplication.class, args);
