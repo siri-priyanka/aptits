80d3fa4afc53c9ca68417190be0c7bd137889891
siri-priyanka <120650732+siri-priyanka@users.noreply.github.com>
2025-07-08 14:24:22 +0530

    Update AptItSolutionsApplication.java

--- src/main/java/com/example/AptItSolutions/AptItSolutionsApplication.java
+++ src/main/java/com/example/AptItSolutions/AptItSolutionsApplication.java
@@ -1,4 +1,4 @@
-package com.example.AptItSolutions;
+package com.aptits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,21 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AptItSolutionsApplication extends SpringBootServletInitializer {
+public class AptitsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(AptItSolutionsApplication.class, args);
-        System.out.println("HELLO APT IT SOLUTIONS");
+        SpringApplication.run(AptitsApplication.class, args);
     }
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(AptItSolutionsApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AptitsApplication.class);
     }
 }
-
-		
-		
-		
-		
-	
